d4489e48cb1e77e6ca1a66f6d4898c9f891190e4
Fadi <mfd4951@rit.edu>
2023-09-06 14:27:19 +0400

    FirstVersion

---
+++ function.py
@@ -0,0 +1,11 @@
+def hello():
+    print("This is a program to print your birthday!")
+
+    name = input("What is your name? \n")
+    month = input("What month is your birthday? \n")
+    day = input("What day of the month is your birthday \n")
+    year = input("What year is your birthday? \n")
+
+    print("Hi",name, ", your birthday is on", month, day, ", the year",year,"." )
+
+# ey joe?
